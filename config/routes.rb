51cd03c187c4a81ca8073f69e86ccff2e8f80884
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-06-08 10:29:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'posts/:id', to: 'posts#show' 
+  # can replace with resource method seen below:
+  resources :posts, only: :show 
 end
